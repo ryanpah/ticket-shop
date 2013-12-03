eef7de6c6afc6df16d7b44b16749d21f97f9604d
Ryan Page <rpage326@gmail.com>
2013-12-03 11:39:31 -0500

    devise migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TicketShop::Application.routes.draw do
+  devise_for :users
   root to: "home#index"
   resources :charges
   # The priority is based upon order of creation: first created -> highest priority.
