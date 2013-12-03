bcdfded02dc32ffdbd15c65f1e48df11f4a34fce
Ryan Page <rpage326@gmail.com>
2013-12-03 09:44:32 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TicketShop::Application.config.secret_key_base = '2667e746ba73ca0abd8bc6af48a50d64b01ce20d25364b787f0e7b06b2e460731142ab89a59ae84de78afba7cca0e547fb733a9fe276aa3a470c3afce38b8dcb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TicketShop::Application.config.session_store :cookie_store, key: '_ticket-shop_session'
