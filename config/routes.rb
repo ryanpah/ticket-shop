63b6d17d2069280c2df0c36d9bd9a745ee0d6d38
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-12-04 12:21:41 -0500

    added routes for charges

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ TicketShop::Application.routes.draw do
   root to: "products#index"
   resources :charges
   resources :products, only: [:index, :show]
+  resources :charges, only: [:new, :create]
 end
