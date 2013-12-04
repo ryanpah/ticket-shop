4e5459de15808e5bfaa15bff08e96705e56713c8
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-12-04 13:32:06 -0500

    configured user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :cart_items
+  has_many :products, through: :cart_items
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
