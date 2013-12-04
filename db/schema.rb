fb33dc7bbc16b8b26e3371de37f77ba89853dd36
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-12-04 14:25:19 -0500

    added company column to product table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131204182719) do
+ActiveRecord::Schema.define(version: 20131204191307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20131204182719) do
     t.text     "images",      array: true
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "company"
   end
 
   create_table "users", force: true do |t|
