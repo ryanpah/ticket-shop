43c5f13a8f34809633935c3ba5317718b52697d8
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-12-04 12:24:28 -0500

    accounting for deleted files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 TicketShop::Application.routes.draw do
-  root to: "products#index"
+  root "products#index"
   resources :charges
   resources :products, only: [:index, :show]
   resources :charges, only: [:new, :create]
